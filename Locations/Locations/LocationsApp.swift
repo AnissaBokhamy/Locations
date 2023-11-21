8dce56542459cda7320505d3038316677dd4f4b0
AnissaBokhamy <25619204+AnissaBokhamy@users.noreply.github.com>
2023-11-20 19:48:49 -0500

    chore: Init XCode project

---
+++ Locations/Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Anissa Bokhamy on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
