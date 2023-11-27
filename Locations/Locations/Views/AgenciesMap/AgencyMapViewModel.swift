894be8b5b9dfa956969b712599e38128d3362b9a
AnissaBokhamy <25619204+AnissaBokhamy@users.noreply.github.com>
2023-11-26 22:13:34 -0500

    feat: Added calculation of center location of agencies for map display

--- Locations/Locations/Views/AgenciesMap/AgencyMapViewModel.swift
+++ Locations/Locations/Views/AgenciesMap/AgencyMapViewModel.swift
@@ -6,9 +6,15 @@ struct AgencyMapViewModel {
   let agency: Agency
   let agenciesMapTheme: AgenciesMapTheme
 
-  // TODO: Change to center location
   var centerLocation: CLLocationCoordinate2D? {
-    return agency.coordinates.maxBounds
+    let coordinates = agency.coordinates
+    guard let minLatitude = coordinates.minLatitude,
+          let maxLatitude = coordinates.maxLatitude,
+          let minLongitude = coordinates.minLongitude,
+          let maxLongitude = coordinates.maxLongitude else { return nil }
+    let latitudeCentre = Double.center(between: minLatitude, and: maxLatitude)
+    let longitudeCentre = Double.center(between: minLongitude, and: maxLongitude)
+    return CLLocationCoordinate2D(latitude: latitudeCentre, longitude: longitudeCentre)
   }
 
   var pinColor: Color {
@@ -26,5 +32,12 @@ struct AgencyMapViewModel {
     default:
       return agenciesMapTheme.defaultPinColor
     }
+  }
+}
+
+fileprivate extension Double {
+
+  static func center(between lhs: Double, and rhs: Double) -> Double {
+    (lhs + rhs) / 2
   }
 }
